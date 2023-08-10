d33a7b88f953192a1c167ab4ba47d5bd54679cc6
Janith <jharinda@gmail.com>
2023-08-10 21:16:35 +0800

    Initial Commit

---
+++ fito-storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fito-storyboard
+//
+//  Created by user234937 on 8/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
